64eae3e8a202939ad79009e753f848ce182ff6c7
AlexTrofim88 <42206009+AlexTrofim88@users.noreply.github.com>
2019-01-26 23:28:13 +0800

    Update 26-01-2019-AlexTrofim88

---
+++ Assets/Script/MusicPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlay : MonoBehaviour {
+
+    private SoundManager soundManager;
+
+	// Use this for initialization
+	void Start () {
+        soundManager = GetComponent<SoundManager>();
+        soundManager.Play("8-bit");
+        DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# QRDeltaPlane
+![QRDeltaPlane icon](https://github.com/MarevoVision/QRDeltaPlane/blob/master/QRDeltaPlane-icon_github.png?raw=true)
+
+QRDeltaPlane.
+
+## Used and applied in the project:
+
+1. Unity
+2. Vuforia
